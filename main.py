8704eab9d86aca8ff73081875e66cf30094517c2
GimykRoot <myk95.gress27@gmail.com>
2025-10-10 22:56:34 +0200

    Update main.py
    Simple update main

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from re import search
-
 from SearchWordU import FileAnalise
 import os
 
-actual_path = os.path.join(os.getcwd(),'Test')
-print(f'Actual path: {actual_path}')
-Works1 = FileAnalise(actual_path)
-task_list = ['list of links','specific link search','search for a word']
+Works1 = FileAnalise()
+task_list = ['List of all links', 'Search specific links', 'Search word']
 task_number = int(input("What do you want to do? You can write only number of function:"))
 if task_number!=1:
     item = input("Word:")
