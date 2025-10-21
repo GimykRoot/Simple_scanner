be1008414816e284b0581d12fa3594a7a4e53fd4
GimykRoot <myk95.gress27@gmail.com>
2025-10-21 13:27:32 +0200

    Update main.py
    updated the main application so that it launches the app

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from re import search
-from SearchWordU import FileAnalise
-import os
+from UserInterFace import FileManagerApp
 
-Works1 = FileAnalise()
-task_list = ['List of all links', 'Search specific links', 'Search word']
-task_number = int(input("What do you want to do? You can write only number of function:"))
-if task_number!=1:
-    item = input("Word:")
-else:
-    item = ''
-Works1.work(task_list[task_number-1],item)
+def main():
+    FileManagerApp().run()
 
+if __name__ == '__main__':
+    main()
